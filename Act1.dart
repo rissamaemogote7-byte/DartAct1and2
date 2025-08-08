9a9998a70cfd16680499bd7bb1cc6fd4b9b082ca
rissamaemogote7-byte <rissamaemogote7@gmail.com>
2025-08-08 09:52:12 +0000

    Activity 1 and 2 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,11 @@
+ void main() {
+  String greeting = "Welcome";
+
+  String user = "Alice";
+
+  print("$greeting, $user!");
+
+  dynamic userDynamic = 25;
+
+  print("$greeting, $userDynamic!");
+ }
